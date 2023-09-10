2ccf5354739e79a56ac013330976927145bc1d13
Mx-Tech <gerhard-maximilian@web.de>
2023-09-10 17:27:56 +0000

    - adding the detail page

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -33,52 +33,28 @@ class _HomePageState extends State<HomePage> {
       body: ListView.builder(
         itemCount: todoList.length,
         itemBuilder: (context, index) {
-          return GestureDetector(
-            onLongPress: () {
-              /// Now we want to make the navigation to the detail page much
-              /// easier, by providing a gesture detector to detect taps, so we
-              /// can navigate to the detail page with a long tap.
-              Navigator.of(context)
-                  .pushNamed(home_page.HomeDetailPage.routeName, arguments: {
-                "todoList": todoList,
-                "index": index
-              }).then((item) {
-                /// This is the return point after popping the detail page!
-                if (item != null && item is TodoItem) {
-                  setState(() {
-                    todoList.replaceRange(index, index + 1, [item]);
-
-                    /// This store items function will save our item on popping
-                    /// the detail route. But we want to achieve saving the item
-                    /// on change.
-                    StorageService().storeTodoItems(todoList);
-                  });
-                }
+          return Dismissible(
+            key: ValueKey<int>(index.hashCode ^ todoList[index].hashCode),
+            onDismissed: (_) {
+              setState(() {
+                todoList.removeAt(index);
+                StorageService().storeTodoItems(todoList);
               });
             },
-            child: Dismissible(
-              key: ValueKey<int>(index.hashCode ^ todoList[index].hashCode),
-              onDismissed: (_) {
+            child: CheckboxListTile(
+              title: Text(
+                todoList[index].title,
+                style: todoList[index].checked
+                    ? const TextStyle(decoration: TextDecoration.lineThrough)
+                    : null,
+              ),
+              value: todoList[index].checked,
+              onChanged: (bool? value) {
                 setState(() {
-                  todoList.removeAt(index);
+                  todoList[index].checked = value ?? false;
                   StorageService().storeTodoItems(todoList);
                 });
               },
-              child: CheckboxListTile(
-                title: Text(
-                  todoList[index].title,
-                  style: todoList[index].checked
-                      ? const TextStyle(decoration: TextDecoration.lineThrough)
-                      : null,
-                ),
-                value: todoList[index].checked,
-                onChanged: (bool? value) {
-                  setState(() {
-                    todoList[index].checked = value ?? false;
-                    StorageService().storeTodoItems(todoList);
-                  });
-                },
-              ),
             ),
           );
         },
@@ -143,26 +119,5 @@ class _HomePageState extends State<HomePage> {
         child: const Icon(Icons.add),
       ),
     );
-  }
-}
-
-class UnperformantList extends StatelessWidget {
-  const UnperformantList({Key? key}) : super(key: key);
-
-  List<String> items = [];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("ToDo It!"),
-        ),
-        body: ListView(
-          children: items
-              .map(
-                (e) => Text(e),
-              )
-              .toList(),
-        ));
   }
 }
