400e55399c7830f721d1cc2d161cb9ea45406622
Mx-Tech <gerhard-maximilian@web.de>
2023-09-20 17:57:33 +0200

    - remove data serialization

--- lib/models/todo_item.dart
+++ lib/models/todo_item.dart
@@ -1,21 +1,22 @@
-import 'package:json_annotation/json_annotation.dart';
-
-/// Currently the file is not available, as we need to generate it via
-/// flutter pub run build_runner build
-part 'todo_item.g.dart';
-
-@JsonSerializable()
 class TodoItem {
-  final String title;
+  String title;
   bool checked;
   String comment;
 
   TodoItem(this.title, this.checked, {this.comment = ""});
 
-  /// Connect the generated [_$TodoItemFromJson] function to the `fromJson`
-  /// factory.
-  factory TodoItem.fromJson(Map<String, dynamic> json) => _$TodoItemFromJson(json);
+  factory TodoItem.fromJson(Map<String, dynamic> json) {
+    String title = json["title"];
+    bool checked = json["checked"];
+    String comment = json["comment"];
+    return TodoItem(title, checked, comment: comment);
+  }
 
-  /// Connect the generated [_$TodoItemToJson] function to the `toJson` method.
-  Map<String, dynamic> toJson() => _$TodoItemToJson(this);
+  Map<String, dynamic> toJson() {
+    return {
+      "title": title,
+      "checked": checked,
+      "comment": comment,
+    };
+  }
 }
