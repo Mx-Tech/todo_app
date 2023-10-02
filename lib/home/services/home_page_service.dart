a6d4f0b82a4d7bfc667237d03414c3534f342566
Mx-Tech <gerhard-maximilian@web.de>
2023-10-02 11:40:44 +0200

    - restructure todo item to match clean code guidelines

--- lib/home/services/home_page_service.dart
+++ lib/home/services/home_page_service.dart
@@ -1,3 +1,4 @@
+import 'package:boringDos/home/pages/detail.dart';
 import 'package:boringDos/models/todo_item.dart';
 import 'package:boringDos/services/storage.dart';
 import 'package:flutter/cupertino.dart';
@@ -9,5 +10,27 @@ class HomePageService{
   HomePageService(){
     todoList = StorageService().getTodoItems();
     todoTitleController = TextEditingController();
+  }
+
+  void navigateToDetailPage(BuildContext context, {required int index}){
+    /// Now we want to make the navigation to the detail page much
+    /// easier, by providing a gesture detector to detect taps. Here we use
+    /// [onLongPress] to navigate to the detail page.
+    Navigator.of(context).pushNamed(HomeDetailPage.routeName, arguments: {
+      "todoList": todoList,
+      "index": index
+    }).then((item) {
+      /// This is the return point after popping the detail page!
+      if (item != null && item is TodoItem) {
+        // TODO: call provider later and rebuild list if list items change
+        todoList
+            .replaceRange(index, index + 1, [item]);
+
+        /// This store items function will save our item on popping
+        /// the detail route. But we want to achieve saving the item
+        /// on change.
+        StorageService().storeTodoItems(todoList);
+      }
+    });
   }
 }
--- lib/home/widgets/item.dart
+++ lib/home/widgets/item.dart
@@ -1,6 +1,4 @@
-import 'package:boringDos/home/pages/detail.dart';
 import 'package:boringDos/home/services/home_page_service.dart';
-import 'package:boringDos/models/todo_item.dart';
 import 'package:boringDos/services/storage.dart';
 import 'package:boringDos/utils/constants.dart';
 import 'package:flutter/material.dart';
@@ -31,27 +29,8 @@ class _TodoItemWidgetState extends State<TodoItemWidget> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onLongPress: () {
-        /// Now we want to make the navigation to the detail page much
-        /// easier, by providing a gesture detector to detect taps. Here we use
-        /// [onLongPress] to navigate to the detail page.
-        Navigator.of(context).pushNamed(HomeDetailPage.routeName, arguments: {
-          "todoList": widget.service.todoList,
-          "index": widget.index
-        }).then((item) {
-          /// This is the return point after popping the detail page!
-          if (item != null && item is TodoItem) {
-            // TODO: call provider later and rebuild list if list items change
-            widget.service.todoList
-                .replaceRange(widget.index, widget.index + 1, [item]);
-
-            /// This store items function will save our item on popping
-            /// the detail route. But we want to achieve saving the item
-            /// on change.
-            StorageService().storeTodoItems(widget.service.todoList);
-          }
-        });
-      },
+      onLongPress: () =>
+          widget.service.navigateToDetailPage(context, index: widget.index),
       child: Dismissible(
         key: ValueKey<int>(widget.index.hashCode ^
             widget.service.todoList[widget.index].hashCode),
@@ -60,47 +39,55 @@ class _TodoItemWidgetState extends State<TodoItemWidget> {
           widget.service.todoList.removeAt(widget.index);
           StorageService().storeTodoItems(widget.service.todoList);
         },
-        background: Container(
-          color: Colors.red,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Icon(
-                Icons.delete,
-                color: Colors.white,
-              ),
-              const Icon(
-                Icons.delete,
-                color: Colors.white,
-              ),
-            ]
-                .map((child) => Padding(
-                      padding: EdgeInsets.all(Paddings.doubled),
-                      child: child,
-                    ))
-                .toList(),
-          ),
-        ),
-        child: ValueListenableBuilder(
-          valueListenable: stateChange,
-          builder: (BuildContext context, bool value, Widget? child) {
-            return CheckboxListTile(
-              title: Text(
-                widget.service.todoList[widget.index].title,
-                style: widget.service.todoList[widget.index].checked
-                    ? const TextStyle(decoration: TextDecoration.lineThrough)
-                    : null,
-              ),
-              value: stateChange.value,
-              onChanged: (bool? value) {
-                widget.service.todoList[widget.index].checked = value ?? false;
-                stateChange.value = value ?? false;
-                StorageService().storeTodoItems(widget.service.todoList);
-              },
-            );
-          },
-        ),
+        background: _background(),
+        child: _item(),
       ),
+    );
+  }
+
+  Container _background() {
+    return Container(
+      color: Colors.red,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+          const Icon(
+            Icons.delete,
+            color: Colors.white,
+          ),
+        ]
+            .map((child) => Padding(
+                  padding: EdgeInsets.all(Paddings.doubled),
+                  child: child,
+                ))
+            .toList(),
+      ),
+    );
+  }
+
+  ValueListenableBuilder<bool> _item() {
+    return ValueListenableBuilder(
+      valueListenable: stateChange,
+      builder: (BuildContext context, bool value, Widget? child) {
+        return CheckboxListTile(
+          title: Text(
+            widget.service.todoList[widget.index].title,
+            style: widget.service.todoList[widget.index].checked
+                ? const TextStyle(decoration: TextDecoration.lineThrough)
+                : null,
+          ),
+          value: stateChange.value,
+          onChanged: (bool? value) {
+            widget.service.todoList[widget.index].checked = value ?? false;
+            stateChange.value = value ?? false;
+            StorageService().storeTodoItems(widget.service.todoList);
+          },
+        );
+      },
     );
   }
 }
